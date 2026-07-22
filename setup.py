0a4929752c75b98d84a14d4282d20ae9f065453b
hardikkaurani <hardikkaurani1@gmail.com>
2026-07-22 13:07:35 +0530

    build: configure package metadata and dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT = '-e .'
+HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
-    '''This function returns a list of requirements'''
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
@@ -12,3 +11,12 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     return requirements
+
+setup(
+    name="HeartDiseasePrediction",
+    version="0.0.1",
+    author="Hema_Kalyan",
+    author_email="kalyanmurapaka274@gmail.com",
+    install_requires=get_requirements("requirements.txt"),
+    packages=find_packages()
+)
