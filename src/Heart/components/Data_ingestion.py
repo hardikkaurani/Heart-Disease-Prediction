d37aab2d653581ed6e286bbf24ae4888338528fc
hardikkaurani <hardikkaurani1@gmail.com>
2026-07-22 13:08:06 +0530

    feat: create DataIngestionConfig class with artifact paths

---
+++ src/Heart/components/Data_ingestion.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+from dataclasses import dataclass
+
+class DataIngestionConfig:
+    raw_data_path:str = os.path.join("Artifacts","raw_data.csv")
+    train_data_path:str = os.path.join("Artifacts","train_data.csv")
+    test_data_path:str = os.path.join("Artifacts","test_data.csv")
