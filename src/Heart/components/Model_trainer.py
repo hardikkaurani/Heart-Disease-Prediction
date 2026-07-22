23966f24a247afed74f82dea2581ec3559ba0b16
hardikkaurani <hardikkaurani1@gmail.com>
2026-07-22 13:08:16 +0530

    feat: create ModelTrainerConfig and ModelTrainer class skeleton

--- src/Heart/components/Model_trainer.py
+++ src/Heart/components/Model_trainer.py
@@ -5,3 +5,11 @@ from dataclasses import dataclass
 from src.Heart.exception import customexception
 from src.Heart.logger import logging
 from src.Heart.utils.utils import save_object, evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("Artifacts", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
