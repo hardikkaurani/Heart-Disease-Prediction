ae96856a7d8644e110ac0977a62646327216da8b
hardikkaurani <hardikkaurani1@gmail.com>
2026-07-22 13:08:28 +0530

    feat: add home route '/' rendering index.html template

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ from src.Heart.pipeline.Prediction_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
 app = application
+
+@app.route('/')
+def index():
+    return render_template('index.html')
