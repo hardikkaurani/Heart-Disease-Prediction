07965ea393a9be6e6ee2483d42544396973b6e7e
hardikkaurani <hardikkaurani1@gmail.com>
2026-07-22 13:08:18 +0530

    feat: implement eval_metrics calculating accuracy, precision, recall, and f1

--- src/Heart/components/Model_evaluation.py
+++ src/Heart/components/Model_evaluation.py
@@ -13,3 +13,11 @@ from src.Heart.utils.utils import load_object
 class ModelEvaluation:
     def __init__(self):
         pass
+
+    def eval_metrics(self, actual, pred):
+        from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+        acc = accuracy_score(actual, pred)
+        precision = precision_score(actual, pred)
+        recall = recall_score(actual, pred)
+        f1 = f1_score(actual, pred)
+        return acc, precision, recall, f1
