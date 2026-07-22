7ceac161c9f5a85dccdfb5875e13fe993d09a6a4
hardikkaurani <hardikkaurani1@gmail.com>
2026-07-22 13:07:46 +0530

    feat: implement save_object serialization utility function

--- src/Heart/utils/utils.py
+++ src/Heart/utils/utils.py
@@ -6,3 +6,12 @@ import pandas as pd
 
 from src.Heart.exception import customexception
 from src.Heart.logger import logging
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+    except Exception as e:
+        raise customexception(e, sys)
