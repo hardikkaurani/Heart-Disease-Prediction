ddf240711137a83f639261eaeee50fd2940291a4
hardikkaurani <hardikkaurani1@gmail.com>
2026-07-22 13:08:16 +0530

    feat: define classification algorithms dictionary in ModelTrainer

--- src/Heart/components/Model_trainer.py
+++ src/Heart/components/Model_trainer.py
@@ -1,3 +1,7 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 import os
 import sys
 from dataclasses import dataclass
@@ -13,3 +17,21 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+            models = {
+                "Random Forest": RandomForestClassifier(),
+                "Decision Tree": DecisionTreeClassifier(),
+                "Logistic Regression": LogisticRegression(),
+                "SVC": SVC()
+            }
+            return models
+        except Exception as e:
+            raise customexception(e, sys)
