6711e24ae1341bbc67e69d9c98768f170c44e1d5
hardikkaurani <hardikkaurani1@gmail.com>
2026-07-22 13:08:09 +0530

    feat: fit and transform train/test input features using preprocessor

--- src/Heart/components/Data_transformation.py
+++ src/Heart/components/Data_transformation.py
@@ -36,6 +36,15 @@ class DataTransformation:
             train_df = pd.read_csv(train_path)
             test_df = pd.read_csv(test_path)
             preprocessing_obj = self.get_data_transformer_object()
-            return train_df, test_df
+            target_column_name = 'target'
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+            return input_feature_train_arr, input_feature_test_arr
         except Exception as e:
             raise customexception(e, sys)
