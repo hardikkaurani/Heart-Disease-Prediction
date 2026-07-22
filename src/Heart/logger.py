c15f2b8b6bdc4765f07523ffe9eb8967606d0ca9
hardikkaurani <hardikkaurani1@gmail.com>
2026-07-22 13:07:46 +0530

    feat: complete logging module setup with test log assertion

--- src/Heart/logger.py
+++ src/Heart/logger.py
@@ -1,14 +1,18 @@
-import logging
 import os
-from datetime import datetime
+import logging
+from datetime import datetime 
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
+LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
+log_path = os.path.join(os.getcwd(), "logs")
+
+os.makedirs(log_path, exist_ok=True)
+
+LOG_FILEPATH = os.path.join(log_path, LOG_FILE)
+
+logging.basicConfig(level = logging.INFO, 
+                    filename = LOG_FILEPATH, 
+                    format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s")
+
+if __name__ == "__main__":
+    logging.info("This is a test log message")
