1d5ab4ff8dd8454001a438c889830ba8ac9615f2
hardikkaurani <hardikkaurani1@gmail.com>
2026-07-22 13:08:18 +0530

    feat: complete ModelTrainer component with logging and params evaluation

--- src/Heart/components/Model_trainer.py
+++ src/Heart/components/Model_trainer.py
@@ -1,59 +1,74 @@
-from sklearn.metrics import accuracy_score, r2_score
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
 import os
 import sys
+import numpy as np
+import pandas as pd
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
 from dataclasses import dataclass
-
-from src.Heart.exception import customexception
 from src.Heart.logger import logging
-from src.Heart.utils.utils import save_object, evaluate_models
+from catboost import CatBoostClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from src.Heart.exception import customexception
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from src.Heart.utils.utils import save_object, evaluate_model
 
-@dataclass
+
+@dataclass 
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("Artifacts", "model.pkl")
-
+    trained_model_file_path = os.path.join('Artifacts','Model.pkl')
+    
+    
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
-
-    def initiate_model_trainer(self, train_array, test_array):
+    
+    def initate_model_training(self,train_array,test_array):
         try:
+            logging.info('Splitting Dependent and Independent variables from train and test data')
             X_train, y_train, X_test, y_test = (
-                train_array[:, :-1],
-                train_array[:, -1],
-                test_array[:, :-1],
-                test_array[:, -1]
-            )
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1])
+            
             models = {
-                "Random Forest": RandomForestClassifier(),
-                "Decision Tree": DecisionTreeClassifier(),
-                "Logistic Regression": LogisticRegression(),
-                "SVC": SVC()
-            }
-            params = {
-                "Random Forest": {'n_estimators': [8, 16, 32, 64, 128]},
-                "Decision Tree": {'criterion': ['gini', 'entropy']},
-                "Logistic Regression": {'C': [0.1, 1.0, 10.0]},
-                "SVC": {'C': [0.1, 1.0, 10.0]}
-            }
-            model_report:dict = evaluate_models(
-                X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models
-            )
+                'Logistic Regression':LogisticRegression(),
+                'Naive Bayes':GaussianNB(),
+                'Random Forest Classfier':RandomForestClassifier(n_estimators=20, random_state=12,max_depth=5),
+                'XG Boost':XGBClassifier(learning_rate=0.01, n_estimators=25, max_depth=15,gamma=0.6, subsample=0.52,colsample_bytree=0.6,
+                                         seed=27, reg_lambda=2, booster='dart', colsample_bylevel=0.6, colsample_bynode=0.5),
+                'K Nearest Neighbors':KNeighborsClassifier(n_neighbors=10),
+                'Decision Tree':DecisionTreeClassifier(criterion = 'entropy',random_state=0,max_depth = 6),
+                'Support Vector Machine':SVC(kernel='rbf', C=2)
+                }
+            
+            model_report = evaluate_model(X_train, y_train, X_test, y_test, models)
+            print(model_report)
+            print('\n====================================================================================\n')
+            logging.info(f'Model Report: {model_report}')
+
+            # To get the best model score from the dictionary
             best_model_score = max(sorted(model_report.values()))
-            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
             best_model = models[best_model_name]
-            if best_model_score < 0.6:
-                raise customexception("No best model found", sys)
-            logging.info(f"Best model found on both training and testing dataset: {best_model_name}")
+
+            print(f'Best Model Found, Model Name: {best_model_name}, Accuracy Score: {best_model_score}')
+            print('\n====================================================================================\n')
+            logging.info(f'Best Model Found, Model Name: {best_model_name}, Accuracy Score: {best_model_score}')
+
             save_object(
-                file_path=self.model_trainer_config.trained_model_file_path,
-                obj=best_model
+                 file_path=self.model_trainer_config.trained_model_file_path,
+                 obj=best_model
             )
-            predicted = best_model.predict(X_test)
-            r2_square = accuracy_score(y_test, predicted)
-            return r2_square
+          
         except Exception as e:
-            raise customexception(e, sys)
+            logging.info('Exception occured at Model Training')
+            raise customexception(e,sys)    
