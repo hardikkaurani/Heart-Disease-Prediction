560a8909e5c6c6853b94f5a547d3d152e449a4a1
hardikkaurani <hardikkaurani1@gmail.com>
2026-07-22 13:08:27 +0530

    feat: implement PredictPipeline predict method with scaling and inference

--- src/Heart/pipeline/Prediction_pipeline.py
+++ src/Heart/pipeline/Prediction_pipeline.py
@@ -4,3 +4,19 @@ import pandas as pd
 from src.Heart.exception import customexception
 from src.Heart.logger import logging
 from src.Heart.utils.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self, features):
+        try:
+            model_path = os.path.join("Artifacts", "model.pkl")
+            preprocessor_path = os.path.join("Artifacts", "preprocessor.pkl")
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+            return preds
+        except Exception as e:
+            raise customexception(e, sys)
