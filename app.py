fbf45f15e809668e5da055fce78c39e12a1c77c4
hardikkaurani <hardikkaurani1@gmail.com>
2026-07-22 13:08:28 +0530

    feat: add predictdata POST route handler skeleton in app.py

--- app.py
+++ app.py
@@ -10,3 +10,13 @@ app = application
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        try:
+            pass
+        except Exception as e:
+            return render_template('error.html', error=str(e))
